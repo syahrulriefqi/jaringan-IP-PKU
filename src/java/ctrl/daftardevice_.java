e3e68c99dcf26eca4c0426892c51cf5c8b0e7243
esyeha <esyeha@esyeha-ThinkPad-T430>
2019-06-20 14:05:44 +0700

    Merge origin/master
    Conflicts:
    	web/index.zul

---
+++ src/java/ctrl/daftardevice_.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ctrl;
+
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+/**
+ *
+ * @author esyeha
+ */
+public class daftardevice_ extends GenericForwardComposer{
+     
+    
+   
+}
+    
+
