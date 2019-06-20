a0952ab818a894d1044de3b99c2396514ff3c16a
esyeha <esyeha@esyeha-ThinkPad-T430>
2019-06-20 10:41:32 +0700

    Merge origin/master
    Conflicts:
    	web/index.zul

--- src/java/ctrl/index_.java
+++ src/java/ctrl/index_.java
@@ -5,20 +5,14 @@
  */
 package ctrl;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
-import org.zkoss.zul.Window;
+
 
 /**
  *
  * @author esyeha
  */
 public class index_ extends GenericForwardComposer{
-    static EntityManagerFactory emf=Persistence.createEntityManagerFactory("jar_pkuPU");
-    static EntityManager em=emf.createEntityManager();
     
-    private Window  winUtamaMenu;
     
 }
